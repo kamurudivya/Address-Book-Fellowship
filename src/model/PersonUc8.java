de9f544d7278657e017a36cbcfdd2405bb6e5c66
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-01-12 01:10:47 +0530

    UC8-ADDED sort by City, State, Zip Functionality

--- src/model/PersonUc8.java
+++ src/model/PersonUc8.java
@@ -3,38 +3,6 @@ package com.bridgelabz.addressbook.models;
 import java.util.Comparator;
 
 public class Person {
-    /*Sort By First Name*/
-    public static Comparator<Person> firstNameSorting = (p1, p2) -> {
-        String firstName = p1.getFirstName();
-        String firstName2 = p2.getFirstName();
-        return firstName.compareToIgnoreCase(firstName2);
-    };
-    /*Sort By City*/
-    public static Comparator<Person> citySorting = (p1, p2) -> {
-        String city1 = p1.getCity();
-        String city2 = p2.getCity();
-        return city1.compareToIgnoreCase(city2);
-    };
-    /*Sort By State*/
-    public static Comparator<Person> stateSorting = (p1, p2) -> {
-        String state1 = p1.getState();
-        String state2 = p2.getState();
-        return state1.compareToIgnoreCase(state2);
-    };
-    /*Sort By Zip*/
-    public static Comparator<Person> zipSorting = (p1, p2) -> {
-        String zip1 = p1.getZip();
-        String zip2 = p2.getZip();
-        return zip1.compareToIgnoreCase(zip2);
-    };
-    private final String firstName;
-    private final String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String phone;
-    private String zip;
-    private String email;
 
     /*Constructor to initialize fields*/
     public Person(String firstName, String lastName, String address, String city, String state, String phone, String zip,String email) {
