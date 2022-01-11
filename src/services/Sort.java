5031794584fecb7de3c195cc3d07b53e7b629660
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-01-12 01:14:21 +0530

    UC8-ADDED sort by City, State, Zip Functionality

--- src/services/Sort.java
+++ src/services/Sort.java
@@ -1,73 +1,40 @@
-package com.bridgelabz.addressbook.services;
+package com.bridgelabz.fellowship.services;
+import com.bridgelabz.fellowship.models.Person;
 
-import com.bridgelabz.addressbook.models.Person;
-import com.bridgelabz.addressbook.utility.InputUtil;
-
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class SearchSortService {
-    public static void searchByCity(List<Person> person) {
-        String search;
-        List<Person> matches = new ArrayList<>();
-        System.out.println("Enter First Name to search : ");
-        search = InputUtil.getStringValue();
-        int flag = 0;
-        for (Person p : person) {
-            if (p.getCity().equalsIgnoreCase(search)) {
-                flag = 1;
-                matches.add(p);
-            }
-        }
-        if (flag == 1) {
-            System.out.println("...Match Found...");
-            for (Person p : matches) {
-                System.out.println(p);
-            }
-        } else {
-            System.out.println("Match Not Found!!!");
+public class Sort {
+    public static void sortByName(List<Person> person)
+    {
+        Collections.sort(person, Person.firstNameSorting);
+        for(Person p: person)
+        {
+            System.out.println(p);
         }
     }
-
-    public static void searchByState(List<Person> person) {
-        String search;
-        int flag = 0;
-        List<Person> matches = new ArrayList<>();
-        System.out.println("Enter First Name to search : ");
-        search = InputUtil.getStringValue();
-        for (Person p : person) {
-            if (p.getState().equalsIgnoreCase(search)) {
-                flag = 1;
-                matches.add(p);
-            }
-        }
-        if (flag == 1) {
-            System.out.println("...Match Found...");
-            for (Person p : matches) {
-                System.out.println(p);
-            }
-        } else {
-            System.out.println("Match Not Found!!!");
+    public static void sortByCity(List<Person> person)
+    {
+        Collections.sort(person, Person.citySorting);
+        for(Person p: person)
+        {
+            System.out.println(p);
         }
     }
-
-    public static void sortByName(List<Person> person) {
-        person.sort(Person.firstNameSorting);
-        person.forEach(System.out::println);
+    public static void sortByState(List<Person> person)
+    {
+        Collections.sort(person, Person.stateSorting);
+        for(Person p: person)
+        {
+            System.out.println(p);
+        }
     }
-
-    public static void sortByCity(List<Person> person) {
-        person.sort(Person.citySorting);
-        person.forEach(System.out::println);
-    }
-
-    public static void sortByState(List<Person> person) {
-        person.sort(Person.stateSorting);
-        person.forEach(System.out::println);
-    }
-
-    public static void sortByZip(List<Person> person) {
-        person.sort(Person.zipSorting);
-        person.forEach(System.out::println);
+    public static void sortByZip(List<Person> person)
+    {
+        Collections.sort(person, Person.zipSorting);
+        for(Person p: person)
+        {
+            System.out.println(p);
+        }
     }
 }
